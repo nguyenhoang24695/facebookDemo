680bef70c94407eac210e7bcde4815f72611fe8c
Hipocrady <napa.nguyenhoang@gmail.com>
2018-04-10 14:53:04 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,92 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express().use(bodyParser.json());
+const request = require('request');
 //
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, response);
 }
 
-// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
 // 
 app.post('/webhook', (req, res) => {
@@ -26,6 +98,13 @@ app.post('/webhook', (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
         res.status(200).send('Event_Recieved');
     } else {
@@ -33,7 +112,7 @@ app.post('/webhook', (req, res) => {
     }
 });
 app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = "EAALIDE94rKMBAKfjW9mxq2pFuYP49Q9auBsVxkkw1qCeo0ZCBoZAajIuJVaaxKznvptSnGsJK4GBmtyZBqJDeu2Df5lInclI1cJZALVeXWGR43BgBAXx4kdPzxQ0c0GW46iZAmpXcvNAoHpO20mL3WKajnJ0UZCUHrFFIDrN5BxAZDZD";
+    let VERIFY_TOKEN = "HJAKHDSSD888ASD656554DASZ";
     // 
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
