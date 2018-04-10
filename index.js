131a82f37df6410625887c30ae289278a2a59499
Hipocrady <napa.nguyenhoang@gmail.com>
2018-04-10 14:10:46 +0700

    update

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express().use(bodyParser.json());
 //
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+// 
 app.post('/webhook', (req, res) => {
     let body = req.body;
     if (body.object === "page") {
         body.entry.forEach(function (entry) {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
         res.status(200).send('Event_Recieved');
     } else {
@@ -16,7 +33,7 @@ app.post('/webhook', (req, res) => {
     }
 });
 app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = "IUDS98ASD9896KJKSD4";
+    let VERIFY_TOKEN = "EAALIDE94rKMBAKfjW9mxq2pFuYP49Q9auBsVxkkw1qCeo0ZCBoZAajIuJVaaxKznvptSnGsJK4GBmtyZBqJDeu2Df5lInclI1cJZALVeXWGR43BgBAXx4kdPzxQ0c0GW46iZAmpXcvNAoHpO20mL3WKajnJ0UZCUHrFFIDrN5BxAZDZD";
     // 
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -38,4 +55,4 @@ app.get('/webhook', (req, res) => {
 })
 // 
 let hostPort = process.env.PORT || 3000;
-app.listen(hostPort, () => (console.log('listening in port ' + hostPort))    );
+app.listen(hostPort, () => (console.log('listening in port ' + hostPort)));
